a6b2b2d8e31083f45d74494a32f9ea26ec8b502b
Asadbek <asadbek@i2max.co.kr>
2025-02-04 18:02:26 +0500

    Stage update -> Component setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-
+import Dashboard from './components/Dashboard';
+import Sidebar from './components/Sidebar';
+import Header from './components/Header';
 
 function App() {
-
   return (
     <>
-      
+      <div className="app">
+        <Header />
+        <div className="main-container">
+          <Sidebar />
+          <Dashboard />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
